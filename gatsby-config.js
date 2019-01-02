7f253a4f4c9bb14c40da5c8e5667025849d20b51
webmcki <webmcki@gmail.com>
2019-01-03 08:35:31 +0900

    게츠비연습

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `게츠비를 공부해 봅시다`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
