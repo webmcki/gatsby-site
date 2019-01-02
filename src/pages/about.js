ac270167397ba6f342ea998966808ae2d4b0f197
webmcki <webmcki@gmail.com>
2019-01-03 08:57:13 +0900

    링크연습

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const about = () => (
+  <Layout>
+    <SEO title="about" />
+    <Link to="/">링크</Link>
+  </Layout>
+)
+
+export default about
---
+++ src/pages/services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const services = () => (
+  <Layout>
+    <SEO title="services" />
+    <Link to="/">링크</Link>
+  </Layout>
+)
+
+export default services
