baefd18d430121d715797930796cdafe6cc85b04
webmcki <webmcki@gmail.com>
2019-01-03 09:11:13 +0900

    네비게이션추가

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Menu from './menu'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -19,6 +20,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <Menu />
         <div
           style={{
             margin: `0 auto`,
---
+++ src/components/menu.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const menu = () => (
+  <div
+    style={{
+      background: '#f4f4f4',
+      paddingTop: '10px',
+    }}
+  >
+    <ul
+      style={{
+        listStyle: 'none',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">about</Link>
+      </li>
+      <li>
+        <Link to="/services">services</Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default menu
